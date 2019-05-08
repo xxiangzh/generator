91d08f1232ae412fb6616dc18f4b58231df97452
xzh <anenan@qq.com>
2019-05-08 11:21:57 +0800

    优化

--- src/main/java/com/xzh/generator/controller/TestController.java
+++ src/main/java/com/xzh/generator/controller/TestController.java
@@ -1,14 +1,9 @@
 package com.xzh.generator.controller;
 
-import com.alibaba.druid.support.json.JSONUtils;
-import com.xzh.generator.mapper.UserMapper;
-import com.xzh.generator.pojo.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.annotation.Resource;
 
 /**
  * @author 向振华
@@ -18,14 +13,10 @@ import javax.annotation.Resource;
 @RequestMapping
 public class TestController {
 
-    @Resource
-    private UserMapper userMapper;
-
     @GetMapping("/test")
     @ResponseBody
     public String test() {
-        User user = userMapper.selectByPrimaryKey(69884L);
-        return JSONUtils.toJSONString(user);
+        return "success";
     }
 
 }
