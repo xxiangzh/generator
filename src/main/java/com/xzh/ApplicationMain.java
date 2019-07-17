9bf2f4149701b81546958ac574d3997de2fa7708
ÏòÕñ»ª <anenan@qq.com>
2019-07-17 16:20:48 +0800

    bugfix

--- src/main/java/com/xzh/ApplicationMain.java
+++ src/main/java/com/xzh/ApplicationMain.java
@@ -1,6 +1,5 @@
 package com.xzh;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2018/9/21 13:16
  */
 @SpringBootApplication
-@MapperScan(basePackages = "com.xzh.generator.mapper")
+//@MapperScan(basePackages = "com.xzh.generator.mapper")
 public class ApplicationMain {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationMain.class, args);
